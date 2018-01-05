faccdd78b8c9c6d71c814d466a73c373b180fa21
Ian Danforth <iandanforth@gmail.com>
2018-01-04 20:54:37 -0800

    guessing part 2; add project .gitignore; remove target/ dir files from tracking

--- book/projects/guessing_game/src/main.rs
+++ book/projects/guessing_game/src/main.rs
@@ -1,9 +1,17 @@
+extern crate rand; // Equivalent to import in python
+
 // Not much is brought in during the prelude, include standard library input/output here
 use std::io;
+use rand::Rng; // this is a 'trait' which must be in scope for random generating method to work
 
 fn main() {
 	// Remember the ! means this is a macro
     println!("Guess the number!");
+
+    let mut rand_generator = rand::thread_rng(); // If this isn't mutable it throws an error. No idea why.
+    let secret_number = rand_generator.gen_range(1, 101); // Half open crap
+    println!("The secret number is {}", secret_number);
+
     println!("Please input your guess.");
     // Create a mutable variable named guess and assign it an instance of a new String object
     let mut guess = String::new();
