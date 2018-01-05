e9e0619749f18e6bcad2c570465c754407768503
Ian Danforth <iandanforth@gmail.com>
2018-01-05 13:55:55 -0800

    Guessing final

--- book/projects/guessing_game/src/main.rs
+++ book/projects/guessing_game/src/main.rs
@@ -2,6 +2,7 @@ extern crate rand; // Equivalent to import in python
 
 // Not much is brought in during the prelude, include standard library input/output here
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng; // this is a 'trait' which must be in scope for random generating method to work
 
 fn main() {
@@ -10,14 +11,38 @@ fn main() {
 
     let mut rand_generator = rand::thread_rng(); // If this isn't mutable it throws an error. No idea why.
     let secret_number = rand_generator.gen_range(1, 101); // Half open crap
-    println!("The secret number is {}", secret_number);
+    // println!("The secret number is {}", secret_number);
+ 
+    // let mut guess = String::new(); Having this outside the loop doesn't work for some reason
+    // crashes with 'ParseIntError { kind: InvalidDigit }'
 
-    println!("Please input your guess.");
-    // Create a mutable variable named guess and assign it an instance of a new String object
-    let mut guess = String::new();
-    // let mut guess = ""; This doesn't work ... no idea why
-    io::stdin().read_line(& mut guess)
-    	.expect("Failed to read line.");
+    loop {
+	    println!("Please input your guess.");
+	    // let mut guess = ""; This doesn't work ... no idea why
 
-    println!("You guessed {}", guess);
+	    // Create a mutable variable named guess and assign it an instance of a new String object
+	    let mut guess = String::new();
+
+	    io::stdin().read_line(& mut guess)
+	    	.expect("Failed to read line.");
+
+	    let guess_num: u32 = match guess.trim().parse() {
+	    	Ok(num) => num,
+	    	Err(_) => , // What is _ and how do I print out the error text?
+	    };
+	    	// .expect("Please enter a number."); // This doesn't seem to create a clean error and crash like in python there is other distracting messaging
+	    	// Also you'll need to know how to either handle or prevent inputs which overflow this type
+
+	    println!("You guessed {}", guess_num);
+
+	    match guess_num.cmp(&secret_number) {
+	    	Ordering::Less => println!("Too small!"),
+	    	Ordering::Greater => println!("Too big!"),
+	    	Ordering::Equal => {
+	    		println!("That's it! Nice work.");
+	    		break;
+	    	}
+	    }
+    }
+
 }
