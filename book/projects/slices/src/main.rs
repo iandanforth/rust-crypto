35e7b8ab5c47c409e4636d94d6be7b3e7ea9e11e
Ian Danforth <iandanforth@gmail.com>
2018-01-15 19:50:03 -0800

    Slices part 2

--- book/projects/slices/src/main.rs
+++ book/projects/slices/src/main.rs
@@ -2,14 +2,22 @@ fn main() {
     // Slices are another data type that does not have ownership
     // 
 
-    let phrase = String::from("Hello, world!");
+    let phrase = String::from("Hello,world!");
 
     let ind = first_word(&phrase);
 
     println!("{:?}", ind );
+
+    // Slicing syntax is the same as python other than using .. rather than :
+    let s = String::from("Hello world");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let b = a[0..3];
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
 
     // To iterate over a string you need to turn it into an array of bytes
     let bytes = s.as_bytes();
@@ -17,9 +25,9 @@ fn first_word(s: &String) -> usize {
     // Here we destructure the tuples returned by enumerate() and assign them to variables
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' { // This is 'byte literal' syntax and is looking for a space
-            return i;
+            return &s[..i];
         }
     }
 
-    return s.len();
+    return &s;
 } 
---
+++ book/questions.md
@@ -0,0 +1,21 @@
+
+
+Why doesn't this work?
+
+```rust
+
+let a: [i32; 5] = [1, 2, 3, 4, 5];
+let b = a[0..3];
+
+```
+
+```
+error[E0277]: the trait bound `[i32]: std::marker::Sized` is not satisfied
+  --> src/main.rs:17:9
+   |
+17 |     let b = a[0..3];
+   |         ^ `[i32]` does not have a constant size known at compile-time
+```
+
+How do you give it a sized trait if not by specifying a size for the array?
+
