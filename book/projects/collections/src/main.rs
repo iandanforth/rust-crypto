370a899661302ad42fb872c452923726550eacf6
Ian Danforth <iandanforth@gmail.com>
2018-02-02 21:13:48 -0800

    Start hashmaps

--- book/projects/collections/src/main.rs
+++ book/projects/collections/src/main.rs
@@ -84,4 +84,26 @@ fn main() {
     let s3 = String::from("toe");
 
     let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    // Hash maps
+
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    // Interestingly this won't compile until you insert something so it can infer types
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 }
