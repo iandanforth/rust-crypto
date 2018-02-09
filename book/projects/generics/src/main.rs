4d3a908dd4338d6bbffd0926290f755aa3d8c1d6
Ian Danforth <iandanforth@gmail.com>
2018-02-08 21:29:34 -0800

    Ok so generics are a little weird

--- book/projects/generics/src/main.rs
+++ book/projects/generics/src/main.rs
@@ -1,44 +1,59 @@
 // Generics are abstract stand-ins for concrete types or other properties
 
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+// fn largest(list: &[i32]) -> i32 {
+//     let mut largest = list[0];
 
-    for &item in list {
-        if item > largest {
-            largest = item
-        }
-    }
+//     for &item in list {
+//         if item > largest {
+//             largest = item
+//         }
+//     }
 
-    return largest
+//     return largest
+// }
+
+// // Generic version of the above
+// fn generic_largest<T>(list: %[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list {
+//         if item > largest {
+//             largest = item
+//         }
+//     }
+
+//     return largest
+// }
+
+// Structs can also have a generic type
+struct Val<T> {
+    val: T
 }
 
-// Generic version of the above
-fn generic_largest<T>(list: %[T]) -> T {
-    let mut largest = list[0];
 
-    for &item in list {
-        if item > largest {
-            largest = item
-        }
-    }
-
-    return largest
+#[derive(Debug)]
+struct Point {
+    x: Val,
+    y: Val
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let num_list = vec![20, 30, 400, 50, 60, 99];
+    // let num_list = vec![20, 30, 400, 50, 60, 99];
 
-    let mut largest = num_list[num_list.len()-1];
+    // let mut largest = num_list[num_list.len()-1];
 
-    for number in num_list {
-        if number > largest {
-            largest = number;
-        }
-    }
+    // for number in num_list {
+    //     if number > largest {
+    //         largest = number;
+    //     }
+    // }
 
-    println!("{:?}", largest);
+    // println!("{:?}", largest);
+
+    let fpoint = Point { x: Val { val: 1.1 } , y: Val { val: 44.33} };
+    // let ipoint = Point { x: 5, y: 10 };
 
 
 }
