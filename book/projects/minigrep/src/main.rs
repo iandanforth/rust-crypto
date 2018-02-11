12fecc9a48e8e06c4e9d6d8d7f0add3b4de2a518
Ian Danforth <iandanforth@gmail.com>
2018-02-10 21:42:19 -0800

    More minigrep

--- book/projects/minigrep/src/main.rs
+++ book/projects/minigrep/src/main.rs
@@ -3,27 +3,27 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
+    let (query, filename) = parse_args(&args);
 
-    if args.len() >= 3 {
-        let query = &args[1];
-        let filename = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 
-        println!("Searching for {}", query);
-        println!("In file {}", filename);
+    let mut f = File::open(filename)
+        .expect("File not found");
 
-        let mut f = File::open(filename)
-            .expect("File not found");
+    let mut contents = String::new();
+    f.read_to_string(& mut contents).
+        expect("Could not read from file");
 
-        let mut contents = String::new();
-        f.read_to_string(& mut contents).
-            expect("Could not read from file");
+    println!("With text:\n{}", contents);
+}
 
-        println!("With text:\n{}", contents);
 
-    } else {
-        println!("At least two arguments <needle> <haystack> are required.");
-    }
+// The rest of the exercise gives bad advice and should be ignored.
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
 
+    return (query, filename);
 }
