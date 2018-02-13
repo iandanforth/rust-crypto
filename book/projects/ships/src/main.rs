aa03d65ef10187ccac416f9e947e4dc11c4569ef
Ian Danforth <iandanforth@gmail.com>
2018-02-12 20:13:53 -0800

    Add more to ships game

--- book/projects/ships/src/main.rs
+++ book/projects/ships/src/main.rs
@@ -1,5 +1,7 @@
 extern crate rand;
 
+use rand::Rng;
+
 fn clear(board: &mut Vec<Vec<i32>>) {
     for i in 0..board.len() {
         let row_len = board[i].len();
@@ -13,6 +15,29 @@ fn show(board: & Vec<Vec<i32>>) {
     for i in 0..board.len() {
         println!("{:?}", board[i]);
     }   
+}
+
+fn set_ships(board: &mut Vec<Vec<i32>>) {
+    let mut s = 0;
+    let num_ships = 5;
+    while s < num_ships {
+
+        let x = rand::thread_rng().gen_range(0, board.len());
+        let y = rand::thread_rng().gen_range(0, board.len());
+        if board[x][y] == 0 {
+            s += 1;
+            board[x][y] = 1;
+        }
+    }
+}
+
+fn attack(board: &mut Vec<Vec<i32>>, x: usize, y: usize) -> bool {
+    if board[x][y] == 1 {
+        board[x][y] = 2;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 fn main() {
@@ -30,6 +55,9 @@ fn main() {
     }
 
     clear(&mut board);
+    show(& board);
+    println!("---------------");
+    set_ships(&mut board);
     show(& board);
 
     println!("Hello, world!");
