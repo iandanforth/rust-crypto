c57596a27d5cf5cbabe6e932cc1c0a7ca1ecdf2c
Ian Danforth <iandanforth@gmail.com>
2018-02-19 19:40:12 -0800

    Make sure I can do fizzbuzz, yup!

---
+++ book/projects/fizzbuzz/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // Print the numbers between 1 and 100
+    for i in 1..101 {
+        if (i % 3 == 0) && (i % 5 == 0) {
+            println!("Fizzbuzz!");
+        } else if i % 3 == 0 {
+            println!("Fizz.");
+        } else if i % 5 == 0 {
+            println!("Buzz.");
+        } else {
+            println!("{:?}", i);
+        }
+    }
+}
