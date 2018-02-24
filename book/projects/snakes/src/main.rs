bee75b8fc2e37b228b05bcf01ed8f477d7272982
Ian Danforth <iandanforth@gmail.com>
2018-02-23 20:58:10 -0800

    Instead of snakes, draw a random bar chart

--- book/projects/snakes/src/main.rs
+++ book/projects/snakes/src/main.rs
@@ -2,6 +2,7 @@ extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
+extern crate rand;
 
 use piston::window::WindowSettings;
 use piston::event_loop::*;
@@ -9,69 +10,90 @@ use piston::input::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::GlGraphics;
 use opengl_graphics::OpenGL;
+use rand::Rng;
 
-pub struct App {
-    gl: GlGraphics,
-    rotation: f64,
-    x: f64,
-    y: f64
-}
-
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
-
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 0.75];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let x = self.x - 50.0;
-        let y = self.y;
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear screen
-            clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-
-            rectangle(RED, square, transform, gl);
-        });
-    }
-}
-
-fn main() {
-    let opengl = OpenGL::V3_2;
-
-    let mut window: Window = WindowSettings::new(
-        "spinning-square",
-        [200, 200]
+fn get_window(opengl: OpenGL, width: u32, height: u32) -> Window {
+    let window: Window = WindowSettings::new(
+        "Chart!",
+        [width, height]
     )
         .opengl(opengl)
         .exit_on_esc(true)
         .build()
         .expect("Things went terribly wrong");
 
-    let mut app = App {
+    return window;
+}
+
+pub struct Graph {
+    gl: GlGraphics,
+    data: Vec<f64>,
+    win_height: u32,
+    win_width: u32
+}
+
+impl Graph {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 0.75];
+
+
+        let data = &self.data;
+        let base = self.win_height as f64;
+        let spacing = 10.0;
+        let left_margin = 0.0;
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear screen
+            clear(WHITE, gl);
+            let mut offset = 0.0;
+            let w = 10.0;
+            for val in data {
+                let h = val.clone();
+                offset += 1.0;
+                let x = left_margin + (offset * (2.0 * w)) + (offset * spacing);
+                let y = base - h;
+                let rect = rectangle::centered([x, y, w, h]);
+                rectangle(RED, rect, c.transform, gl);
+            }
+        });
+    }
+}
+
+
+
+fn main() {
+    let opengl = OpenGL::V3_2;
+
+    let win_height: u32 = 400;
+    let win_width: u32 = 800;
+
+    let mut window = get_window(opengl, win_width, win_height);
+
+    let mut rand_generator = rand::thread_rng(); // If this isn't mutable it throws an error. No idea why.
+    let mut data: Vec<f64> = Vec::new();
+    for _ in 0..25 {
+        let num = rand_generator.gen_range(1, 101) as f64;
+        data.push(num);
+    }
+
+    // let data = vec![20.0, 30.0, 40.0, 50.0, 60.0, 0.0, 1.0];
+
+    let mut graph = Graph {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-        x: 100.0,
-        y: 100.0
+        data: data,
+        win_height: win_height,
+        win_width: win_width
     };
 
     let mut events = Events::new(EventSettings::new());
 
-    if let Some(e) = events.next(&mut window) {
+    // Display the window and spin
+    while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
-                app.render(&r);
+            graph.render(&r);
         }
-    }
-
-
-    let mut counter = 0;
-    loop {
-        counter += 1;
     }
 }
