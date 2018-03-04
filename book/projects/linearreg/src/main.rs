9b837dd357a60f755e19ff3c92804463cf5462f5
Ian Danforth <iandanforth@gmail.com>
2018-03-04 00:44:33 -0700

    Basic csv reading

--- book/projects/linearreg/src/main.rs
+++ book/projects/linearreg/src/main.rs
@@ -1,12 +1,24 @@
-// Linear Regression Translation from matlab into rust
+extern crate csv;
 
+// Linear Regression Translation from matlab into rust
+use std::fs::File;
 
 fn fit() {
     println!("Fitting data ...");
 }
 
+fn load(filename: &'static str) {
+    let file = File::open(filename).expect("Blur blur");
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record = result.expect("Bad line");
+        println!("{:?}", record);
+    }
+
+}
 
 fn main() {
     // Set up data
+    load("ex1data2.txt");
     fit();
 }
