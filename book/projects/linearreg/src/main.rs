e0c1f8afd4bcdb25017b67d4d5604d94fa289fbf
Ian Danforth <iandanforth@gmail.com>
2018-03-08 23:10:45 -0800

    got data loading into DMatrix, now to try to use it

--- book/projects/linearreg/src/main.rs
+++ book/projects/linearreg/src/main.rs
@@ -1,45 +1,73 @@
 extern crate csv;
 extern crate nalgebra as na;
 
-
 // Linear Regression Translation from matlab into rust
 use std::fs::File;
-use na::{Matrix, Dynamic, MatrixArray, MatrixVec, RowDVector};
+use std::iter::Sum;
+use na::{DMatrix, Vector2};
 
 fn fit() {
     println!("Fitting data ...");
 }
 
-fn load(filename: &'static str) {
-    type DMatrixi32 = Matrix<i32, Dynamic, Dynamic, MatrixVec<i32, Dynamic, Dynamic>>;
+fn load(filename: &'static str) -> na::Matrix<f64, na::Dynamic, na::Dynamic, na::MatrixVec<f64, na::Dynamic, na::Dynamic>> {
     let file = File::open(filename).expect("Blur blur");
     let mut rdr = csv::Reader::from_reader(file);
-    let mut rows = Vec::new();
-    let mut colCount = 0;
+    let mut all_vals = Vec::new();
+    let mut col_count = 0;
+    let mut row_count = 0;
+    let mut temp_count = 0;
     for result in rdr.records() {
         let record = result.expect("Bad line");
-        let mut temp = Vec::new();
         for field in record.iter() {
-            let f = field.parse::<i32>().expect("fluber");
-            temp.push(f);
+            println!("{:?}", field);
+            let f = field.parse::<f64>().expect("fluber");
+            all_vals.push(f);
+            temp_count += 1;
         }
-        if colCount == 0 {
-            colCount = temp.len();
+        if col_count == 0 {
+            col_count = temp_count;
         }
-        let rdv = RowDVector::from_iterator(3, temp.iter().cloned());
-        rows.push(rdv);
+        row_count += 1;
     }
-    let rowCount = rows.len();
-    if 0.0 == 0 {
-        println!("I am a sane programming language");
-    }
-    // let m = DMatrixi32::from_iterator(rowCount, colCount, rows.iter().cloned());
 
+    let dmatrix = DMatrix::from_iterator(
+        row_count,
+        col_count,
+        all_vals.iter().cloned()
+    );
 
+    return dmatrix;
+}
+
+fn compute_cost(
+        X: na::Matrix<f64, na::Dynamic, na::Dynamic, na::MatrixVec<f64, na::Dynamic, na::Dynamic>>,
+        y: na::Matrix<f64, na::Dynamic, na::Dynamic, na::MatrixVec<f64, na::Dynamic, na::Dynamic>>
+    ) -> f64 {
+
+    let m = y.len();
+    let scalar = 1 / (2 * m);
+    let theta = Vector2::new(0.0, 0.0);
+    let prod = X * theta;
+    println!("{:?}", prod); 
+    // J = (1/(2*m)) * sum(((X * theta) - y).^2);
+    return 0.0;
 }
 
 fn main() {
     // Set up data
-    load("ex1data2.txt");
+    let data = load("ex1data1.txt");
+    // Pull out the first two columns then add a 1's column
+    let X = data.columns(0, 1).insert_column(0, 1.0);
+    // Pull out the final column as the labels
+    let y = data.columns(1, 1).clone_owned();
+
+    let theta = Vector2::new(0.0, 0.0);
+
+    let iterations = 1500;
+    let alpha = 0.01;
+
+    compute_cost(X, y);
+
     fit();
 }
